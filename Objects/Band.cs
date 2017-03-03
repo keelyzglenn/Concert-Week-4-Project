0563174b2165f198ee26823e486f23c1791f55f6
Keely Glenn <glenj392@newschool.edu>
2017-03-03 08:26:08 -0800

    add get and setters as well as bool override to Band object

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -6,5 +6,53 @@ namespace Concert
 {
     public class Band
     {
+        private int _id;
+        private string _name;
+
+        public Band(string Name, int Id = 0);
+        {
+            _id = Id;
+            _name = Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetName().GetHashCode();
+        }
+
+        // id
+        public bool GetId()
+        {
+            return _id;
+        }
+
+        // name getter and setter
+        public bool GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        // stops doubles between database and object
+        public override bool Equals(System.Object otherBand)
+        {
+            if (!(otherBand is Band))
+            {
+                return false;
+            }
+            else
+            {
+                Bnad newBand = (Band) otherBand;
+                bool idEquality = this.GetId() == newBand.GetId();
+                bool nameEquality = this.GetName() == newBand.GetName();
+                return (idEquality && nameEquality);
+            }
+        }
+
+
     }
 }
